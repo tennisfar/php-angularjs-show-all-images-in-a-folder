a21d608e36b2b0e16a243dd11f8312fb5c87a1e0
Mike Lothar <mikelothar@gmail.com>
2015-01-24 14:31:27 +0100

    Added Angular, refactored file location

---
+++ app/php-to-js-array.php
@@ -0,0 +1,10 @@
+<?php
+    echo "var images = {};\n";
+    $all_images = glob("img/{*.jpg, *.JPG, *.JPEG, *.png, *.PNG}", GLOB_BRACE);
+    $count = count($all_images);
+    for ($i = 0; $i < $count; $i++) {
+        echo "images[".$i."] = {};\n";
+        echo "images[".$i."].src = '".$all_images[$i]."';\n";
+        echo "images[".$i."].modified = '".date('YmdHis', filemtime($all_images[$i])).$i."';\n";
+    }
+?>
