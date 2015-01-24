2110bfbac30c046791d966a6f49ff46f05475e44
Mike Lothar <mikelothar@gmail.com>
2015-01-24 16:34:55 +0100

    Added filter. Added name

--- app/php-to-js-array.php
+++ app/php-to-js-array.php
@@ -1,11 +1,13 @@
 <?php
     header('Content-Type: text/javascript; charset=UTF-8');
     echo "var images = [];\n";
-    $all_images = glob("img/{*.jpg, *.JPG, *.JPEG, *.png, *.PNG}", GLOB_BRACE);
+    $folder = 'img/';
+    $all_images = glob($folder."{*.jpg, *.JPG, *.JPEG, *.png, *.PNG}", GLOB_BRACE);
     $count = count($all_images);
     for ($i = 0; $i < $count; $i++) {
         echo "images[".$i."] = {};\n";
         echo "images[".$i."].src = '".$all_images[$i]."';\n";
+        echo "images[".$i."].name = '".substr($all_images[$i],strlen($folder),strpos($all_images[$i], '.')-strlen($folder))."';\n";
         echo "images[".$i."].modified = '".date('YmdHis', filemtime($all_images[$i])).$i."';\n";
     }
 ?>
