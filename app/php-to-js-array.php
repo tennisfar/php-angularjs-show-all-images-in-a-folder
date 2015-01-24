38f956145cd61bd672a104536b27f0af73b8a9f3
Mike Lothar <mikelothar@gmail.com>
2015-01-24 15:59:24 +0100

    Fixed object, should be array

--- app/php-to-js-array.php
+++ app/php-to-js-array.php
@@ -1,5 +1,6 @@
 <?php
-    echo "var images = {};\n";
+    header('Content-Type: text/javascript; charset=UTF-8');
+    echo "var images = [];\n";
     $all_images = glob("img/{*.jpg, *.JPG, *.JPEG, *.png, *.PNG}", GLOB_BRACE);
     $count = count($all_images);
     for ($i = 0; $i < $count; $i++) {
